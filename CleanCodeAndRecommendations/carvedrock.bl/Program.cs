0ff26dab115021823bea267992c71c1e74838012
Josvany_Garcia <josvanyg@gmail.com>
2023-07-21 12:26:01 -0600

    implement dry

--- CleanCodeAndRecommendations/carvedrock.bl/Program.cs
+++ CleanCodeAndRecommendations/carvedrock.bl/Program.cs
@@ -1,4 +1,16 @@
 ﻿
 using carvedrock.bl.principles;
+using carvedrock.bl.principles.DRY;
 
+Dry dy = new();
+
+Console.WriteLine("Kiss -  keep it simple, stupid");
 Console.WriteLine(new KISS().GetMonth(8));
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Dry - Don't Repeat Yourself");
+
+Console.WriteLine("BackPack Price: " + dy.backpack.Price.ToString());
+Console.WriteLine("Shoes Price: " + dy.shoes.Price.ToString());
---
+++ CleanCodeAndRecommendations/carvedrock.bl/principles/DRY/Dry.cs
@@ -0,0 +1,30 @@
+﻿namespace carvedrock.bl.principles.DRY
+{
+    /// <summary>
+    /// DRY = Don't Repeat Yourself
+    ///     = No te repitas
+    /// </summary>
+    internal class Dry
+    {
+        public BackPack backpack = new("test", 150, 85, 200, true);
+        public Shoes shoes = new("test shoes", 199, 10, "Black", true);
+
+        public Dry()
+        {
+            backpack.Price = PriceWithDiscount(backpack.Price);
+            shoes.Price = PriceWithDiscount(shoes.Price);
+        }
+        public static double PriceWithDiscount(double price)
+        {
+            if ((price > 100) && (price < 400))
+            {
+                price -= price * 0.30;
+            }
+            else if (price >= 400)
+            {
+                price -= price * 0.60;
+            }
+            return price;
+        }
+    }
+}
